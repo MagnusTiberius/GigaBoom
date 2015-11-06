7d1b3680083080d83c0fd678fae8cc10c0924c88
bgonzales20001 <bgonzales20001@gmail.com>
2015-11-06 12:38:32 -0800

    update address

--- LocationLib/Address.cs
+++ LocationLib/Address.cs
@@ -8,6 +8,7 @@ namespace LocationLib
 {
     public class Address
     {
+        public int AddressID { get; set; }
         public string Address1 { get; set; }
         public string Address2 { get; set; }
         public string City { get; set; }
